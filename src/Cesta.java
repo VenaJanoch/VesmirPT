bfd840dec0a12d55a20e18fc825431d4a3c3c495
Filip Kupilík <Filip@147.228.135.196>
2015-10-17 16:36:19 +0200

    Stejný jako master

--- src/Cesta.java
+++ src/Cesta.java
@@ -1,4 +1,28 @@
 
 public class Cesta {
+	
+	private Planeta pocatecni;
+	private Planeta koncova;
+	private int vzdalenos;
+	
+	
+	public Planeta getPocatecni() {
+		return pocatecni;
+	}
+	public void setPocatecni(Planeta pocatecni) {
+		this.pocatecni = pocatecni;
+	}
+	public Planeta getKoncova() {
+		return koncova;
+	}
+	public void setKoncova(Planeta koncova) {
+		this.koncova = koncova;
+	}
+	public int getVzdalenos() {
+		return vzdalenos;
+	}
+	public void setVzdalenos(int vzdalenos) {
+		this.vzdalenos = vzdalenos;
+	}
 
 }
--- src/Vesmir.java
+++ src/Vesmir.java
@@ -7,6 +7,8 @@ public class Vesmir {
 	public static void main(String[] args) {
 		Rozdeleni o = new Rozdeleni(5000, 3000000, 0.2,
 													100000, 10000000, 800,800);
+		Graf g = new Graf(o.getPlanety());
+		
 		
 	}
 }

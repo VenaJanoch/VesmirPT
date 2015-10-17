5bea6d215075c1cfdd63c04856374c019f421cde
Filip Kupilík <Filip@147.228.135.196>
2015-10-17 17:22:06 +0200

    blbost

--- Vesmir.java
+++ Vesmir.java
@@ -1,10 +1,13 @@
 
 
 
+
+
 public class Vesmir {
 
 	
 	public static void main(String[] args) {
+		System.out.println("bla");
 		Rozdeleni o = new Rozdeleni(5000, 3000000, 0.2,
 													100000, 10000000, 800,800);
 		Graf g = new Graf(o.getPlanety());
